c9c2b999cd9432824a11b6bef85608f03204ac06
Subham <shubhamkrp209@gmail.com>
2021-03-14 18:04:16 +0530

    Add reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const store=createStore(movies);
+console.log(store);
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
