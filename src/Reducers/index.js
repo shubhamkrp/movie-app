fa413c7efe42ff19032bf5475af563a4c0167973
Subham <shubhamkrp209@gmail.com>
2021-03-15 19:33:57 +0530

    add combineReducers

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,3 +1,5 @@
+import {combinereducers} from 'redux';
+
 import {ADD_MOVIES,ADD_TO_FAVORITES,REMOVE_FROM_FAVORITES,SET_SHOW_FAVORITES} from '../actions';
 
 const initialMovieState={
@@ -6,7 +8,7 @@ const initialMovieState={
 	showFavorites:false
 }
 
-export default function movies(state=initialMovieState, action){
+export function movies(state=initialMovieState, action){
 	
 	switch(action.type){
 		case ADD_MOVIES:
@@ -36,3 +38,28 @@ export default function movies(state=initialMovieState, action){
 		return state;
 	}
 }
+
+const initialSearchState={
+	result:{}
+};
+
+export function search(state=initialSearchState,action){
+	return state;
+}
+
+const initialRootState={
+	movies:initialMovieState,
+	search:initialSearchState
+}
+
+/*export default function rootReducer(state=initialRootState,action){
+	return{
+		movies:movies(state.movies,action),
+		search:search(state.search,action)
+	}
+}*/
+
+export default combinereducers({
+	movies: movies, //can use movies only in place of movies: movies, since both have same name
+	search: search
+});
--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     const {store} = this.props;
 
     store.subscribe(()=>{
-      console.log('UPDATED');
+      //console.log('UPDATED');
       this.forceUpdate();
     });
     //dispatch action
@@ -19,8 +19,8 @@ class App extends React.Component {
   }
 
 isMovieFavorite =(movie) =>{
-  const {favorites} =this.props.store.getState();
-  const index=favorites.indexOf(movie);
+  const {movies} =this.props.store.getState();
+  const index=movies.favorites.indexOf(movie);
   if(index!==-1) return true;
   return false;
 }
@@ -28,7 +28,8 @@ onChangeTab=(val) => {
   this.props.store.dispatch(setShowFavorites(val));
 }
   render(){
-    const {list,favorites,showFavorites}=this.props.store.getState();
+    const {movies}=this.props.store.getState();
+    const {list,favorites,showFavorites}=movies;
     //console.log(this.props.store.getState());
     const displayMovies=showFavorites?favorites:list;
     return (
@@ -36,8 +37,12 @@ onChangeTab=(val) => {
         <NavBar/>
         <div className="main">
           <div className="tabs">
-            <div className={`tab ${showFavorites?'':'active-tabs'}`} onClick={()=>this.onChangeTab(false)}> Movies </div>
-            <div className={`tab ${showFavorites?'active-tabs':''}`} onClick={()=>this.onChangeTab(true)}> Favourites </div>
+            <div 
+            className={`tab ${showFavorites?'':'active-tabs'}`} 
+            onClick={()=>this.onChangeTab(false)}> Movies </div>
+            <div 
+            className={`tab ${showFavorites?'active-tabs':''}`} 
+            onClick={()=>this.onChangeTab(true)}> Favourites </div>
           </div>
 
           <div>
